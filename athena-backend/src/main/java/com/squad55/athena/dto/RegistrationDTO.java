e325809e845a7f6b4f8f64d91ed5b4ca445f47fb
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-03-14 15:32:27 -0300

    RegistrationDTO

---
+++ athena-backend/src/main/java/com/squad55/athena/dto/RegistrationDTO.java
@@ -0,0 +1,30 @@
+package com.squad55.athena.dto;
+
+public class RegistrationDTO {
+	
+	private Long courseId;
+	private Long email;
+	
+	public RegistrationDTO() {
+		
+	}
+
+	public Long getCourseId() {
+		return courseId;
+	}
+
+	public void setCourseId(Long courseId) {
+		this.courseId = courseId;
+	}
+
+	public Long getEmail() {
+		return email;
+	}
+
+	public void setEmail(Long email) {
+		this.email = email;
+	}
+	
+	
+
+}

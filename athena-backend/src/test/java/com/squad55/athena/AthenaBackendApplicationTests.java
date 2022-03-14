74451739f998b7fe15fc0fe44d7b162e09859b76
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-03-14 12:08:24 -0300

    initial setting

---
+++ athena-backend/src/test/java/com/squad55/athena/AthenaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.squad55.athena;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthenaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

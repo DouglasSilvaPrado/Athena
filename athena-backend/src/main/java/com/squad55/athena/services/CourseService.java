e26b4164a168a34963db0b36e4fea8cec1b8cce2
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-03-14 14:48:09 -0300

    course service

---
+++ athena-backend/src/main/java/com/squad55/athena/services/CourseService.java
@@ -0,0 +1,27 @@
+package com.squad55.athena.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.squad55.athena.dto.CourseDTO;
+import com.squad55.athena.entities.Course;
+import com.squad55.athena.repositories.CourseRepository;
+
+@Service
+public class CourseService {
+	
+	@Autowired
+	private CourseRepository repository;
+	
+
+	@Transactional(readOnly = true)
+	public Page<CourseDTO> findAll(Pageable pageable) {
+		Page<Course> result = repository.findAll(pageable);
+		Page<CourseDTO> page = result.map(x -> new CourseDTO(x));
+		return page;
+	}
+
+}

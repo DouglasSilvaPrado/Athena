81d831581ba81efe38066c176e7059c1397a0e35
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-03-14 19:52:52 -0300

    routes, navbar

--- athena-frontend/src/App.tsx
+++ athena-frontend/src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/home";
 
 function App() {
   return (
-    <div className="App">
-      
-       
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 

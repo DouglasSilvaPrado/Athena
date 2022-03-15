aae2f84914d18dced26659b72943a76be1d2d050
DouglasSilvaPrado <douglassilvadaprado@gmail.com>
2022-03-15 12:12:56 -0300

    course card

---
+++ athena-frontend/src/components/layout/CourseCard/index.tsx
@@ -0,0 +1,24 @@
+function CourseCard() {
+    const course = {
+        id:1,
+        image: "https://robertmarshall.dev/static/d71528d312b579dd0449078f8a8f56ab/b5380/reactjs-1.png",
+        name: "React",
+        description: "Curso de React basico",
+        duration: 7
+    }
+    return ( 
+        <>
+            <div className="card w-100" >
+                <img src={course.image} className="card-img-top" alt={course.name}/>
+                <div className="card-body">
+                    <h5 className="card-title">{course.name}  {`[${course.duration} Horas]`}</h5>
+                    <p className="card-text">{course.description}</p>
+                    <a href="#" className="btn btn-primary">Começar</a>
+                </div>
+            </div>
+            
+        </>
+     );
+}
+
+export default CourseCard;
